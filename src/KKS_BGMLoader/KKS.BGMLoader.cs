3e4ead862e3c441defd927f9f76e7be3d6e41da2
zyf18923 <53826821+zyf18923@users.noreply.github.com>
2025-06-02 14:04:57 +0800

    Update KKS.BGMLoader.cs
    BGMLoader功能修改为：重定向音效文件

--- src/KKS_BGMLoader/KKS.BGMLoader.cs
+++ src/KKS_BGMLoader/KKS.BGMLoader.cs
@@ -14,14 +14,13 @@ namespace BGMLoader
             overrideFileName = null;
 
             //kks_song_00 or kks_bgm_00
-            var isBgm = context.Parameters.Name.Length == 10 && context.Parameters.Name.StartsWith("kks_bgm", System.StringComparison.InvariantCultureIgnoreCase) ||
-                        context.Parameters.Name.Length == 11 && context.Parameters.Name.StartsWith("kks_song", System.StringComparison.InvariantCultureIgnoreCase);
+            var isBgm = context.Parameters.Name.Length == 9 && context.Parameters.Name.StartsWith("hse_ks", System.StringComparison.InvariantCultureIgnoreCase);
             if (!isBgm) return false;
 
             var parts = context.Parameters.Name.Split('_');
             if (parts.Length != 3) return false;
 
-            overrideFileName = $"{parts[1].ToUpper()}{int.Parse(parts[2]):00}.ogg";
+            overrideFileName = $"{parts[1].ToUpper()}{int.Parse(parts[2]):00}.wav";
             return true;
         }
     }
